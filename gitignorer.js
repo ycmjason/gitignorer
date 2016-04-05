5046763266deba04d416cf95d1ac0ca096e1a3f6
Jason Yu <ycm.jason@gmail.com>
2016-04-05 11:57:02 +0100

    added basic commands for commander

--- gitignorer.js
+++ gitignorer.js
@@ -5,40 +5,40 @@ var program = require('commander');
 var handlers = require('./lib/handlers'); 
 
 program
-  .version(require('./package.json').version)
+  .version(require('./package.json').version);
+
+program
+  .command('init [profile]')
+  .description('Create .gitignore at current directory')
+  .action(handlers.init);
  
 program
   .command('add <files...>')
   .description('Add file(s) to .gitignore')
-  .option('-t, --tag [tag]', 'add file to tag instead of .gitignore')
-  .action(handlers.ignore);
+  .option('-t, --profile [profile]', 'add file to profile instead of .gitignore')
+  .action(handlers.add);
 
 program
   .command('remove <files...>')
   .alias('rm')
-  .description('Remove ignored file from .gitignore or [tag]')
-  .option('-t, --tag [tag]', 'which tag to list')
+  .description('Remove ignored file from .gitignore or [profile]')
+  .option('-t, --profile [profile]', 'which profile to list')
   .action(handlers.remove);
  
 program
   .command('list')
   .alias('ls')
-  .description('List ignored file in .gitignore or [tag]')
-  .option('-t, --tag [tag]', 'which tag to list')
+  .description('List ignored file in .gitignore or [profile]')
+  .option('-t, --profile [profile]', 'which profile to list')
   .action(handlers.list);
 
 program
-  .command('tag <name>')
-  .description('Perform operation on tag base on [options]')
-  .option('-a, --add', 'create a tag')
-  .option('-d, --delete', 'delete a tag')
-  .option('-cp, --copy <destination>', 'copy a tag to <destination> with all the settings copied')
-  .option('-mv, --rename <destination>', 'rename a tag with all the settings remain')
-  .action(handlers.tag);
+  .command('profile <name>')
+  .description('Perform operation on profile base on [options]')
+  .option('-a, --add', 'create a profile')
+  .option('-d, --delete', 'delete a profile')
+  .option('-cp, --copy <destination>', 'copy a profile to <destination> with all the settings copied')
+  .option('-mv, --rename <destination>', 'rename a profile with all the settings remain')
+  .action(handlers.profile);
 
 program.parse(process.argv);
-
-if (typeof cmdValue === 'undefined') {
-  console.log(program.help());
-  process.exit(1);
-}
